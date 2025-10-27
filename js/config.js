6c2017b18edbe744ef9a56869b3df3abcd9fd964
Ravi Prajapati <34765312+ravi199mu@users.noreply.github.com>
2025-10-27 14:45:59 +0530

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_91ya4vnkq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4a26lkc3r9in7tkm12tqin9t8v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Nulz9GTOe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '43ar07b9r28g1am3megmdfu580', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
